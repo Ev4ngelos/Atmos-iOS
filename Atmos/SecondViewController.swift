1aebe68b3453f32375b7caea0454443e220e32af
Evangelos <niforatos@gmail.com>
2016-01-02 12:04:34 +0200

    Initial Commit

---
+++ Atmos/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Atmos
+//
+//  Created by Evangelos on 02/01/16.
+//  Copyright © 2016 Evangelos. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
